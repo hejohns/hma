fd1b1b2db3c9124c7012515f2b196043ff50b54e
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-07 01:25:21 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,4 +1,25 @@
 <html>
+  <head>
+    <style>
+      a.button {
+        -webkit-appearance: button;
+        -moz-appearance: button;
+        appearance: button;
+        text-decoration: none;
+        color: initial;
+        }
+      body {
+        background-color: ivory;
+        text-align: center;
+        }
+      p {
+        text-align: center;
+        }
+     form {
+      display: inline-block;
+      }
+      </style>
+  </head>
   <?php
     echo "form.php under construction."; 
   ?>
