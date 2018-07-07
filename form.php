86b865f7e4dbe971bd41fd5209a546c586740785
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-07 01:48:40 -0400

    Update form.php

--- form.php
+++ form.php
@@ -24,6 +24,11 @@
     <?php
       echo "form.php under construction."; 
       echo "<p>" . $_GET["init"] . $_GET["name"] . $_GET["size"] . "</p>";
+      $name = htmlspecialchars($_GET["name"], ENT_QUOTES);
+      $size = htmlspecialchars($_GET["size"], ENT_QUOTES);
+      if ($_GET["init"] == 1){
+        echo $name . $size;
+        }
     ?>
   </body>
 </html>
