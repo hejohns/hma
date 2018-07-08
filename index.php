2ff81b860ceca6035a678ee911f511ccbb26aad4
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-08 03:17:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,9 +11,16 @@ $user = $_POST['usern'];
 $pass = $_POST['passw'];
 
 if($user == $indexUsername
-&& $pass == $indexPassword)
+&& $pass == $indexPassword
+&& $_POST['indexInit'] == 1)
 {
         include("../main.php");
+}
+if($user == $indexUsername
+&& $pass == $indexPassword
+&& $_POST['indexInit'] == 2)
+{
+        include("../form.php");
 }
 else
 {
@@ -22,6 +29,7 @@ else
 
 echo '
             <form method="POST" action="index.php">
+            <input type="hidden" name="indexInit" value="1" />
             User <input type="text" name="usern"></input><br>
             Pass <input type="password" name="passw"></input><br>
             <input type="submit" name="submit" value="Submit"></input>
