5c4fad09b40c758757f88036f7868552c128c8f1
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-08 03:08:54 -0400

    Update main.php

--- main.php
+++ main.php
@@ -6,20 +6,20 @@
       include "config.php";
       echo '
       <form action="index.php" method="POST">
-        <input type="hidden" name="usern" value=' . "$indexUsername" . '/>
-        <input type="hidden" name="passw" value=' . "$indexPassword" . '/>
+        <input type="hidden" name="usern" value="' . "$indexUsername" . '"/>
+        <input type="hidden" name="passw" value="' . "$indexPassword" . '"/>
         <input type="hidden" name="value" value="0"/>
         <button name ="submit" value="1" type="submit">Home</button>
       </form>
       <form action="index.php" method="POST">
-        <input type="hidden" name="usern" value=' . "$indexUsername" . '/>
-        <input type="hidden" name="passw" value=' . "$indexPassword" . '/>
+        <input type="hidden" name="usern" value="' . "$indexUsername" . '"/>
+        <input type="hidden" name="passw" value="' . "$indexPassword" . '"/>
         <input type="hidden" name="value" value="1"/>
         <button name ="submit" value="1" type="submit">List all entries</button>
       </form>
       <form action="index.php" method="POST">
-        <input type="hidden" name="usern" value=' . "$indexUsername" . '/>
-        <input type="hidden" name="passw" value=' . "$indexPassword" . '/>
+        <input type="hidden" name="usern" value="' . "$indexUsername" . '"/>
+        <input type="hidden" name="passw" value="' . "$indexPassword" . '"/>
         <input type="hidden" name="value" value="2"/>
         <button name ="submit" value="1" type="submit">Number of entries</button>
       </form>      
