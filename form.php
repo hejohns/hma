1747c8216311b0dd3ed57714a2519c475feda177
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-09 00:45:16 -0400

    Update form.php

--- form.php
+++ form.php
@@ -1,11 +1,8 @@
 <html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
-  </head>
   <body>
     <?php
       echo "form.php under construction."; 
-      echo "<p>" . $_GET["init"] . $_GET["name"] . $_GET["size"] . "</p>";
+      echo "<p>" . $_POST["init"] . $_POST["name"] . $_POST["size"] . "</p>";
       $name = htmlspecialchars($_POST["name"], ENT_QUOTES);
       $size = htmlspecialchars($_POST["size"], ENT_QUOTES);
       if ($_POST["init"] == 1){
