4be0e23e8e0832351fa158b0948b4bd71dea1536
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-09 00:46:32 -0400

    Update form.php

--- form.php
+++ form.php
@@ -2,10 +2,10 @@
   <body>
     <?php
       echo "form.php under construction."; 
-      echo "<p>" . $_POST["init"] . $_POST["name"] . $_POST["size"] . "</p>";
+      echo "<p>" . $_POST["formInit"] . $_POST["name"] . $_POST["size"] . "</p>";
       $name = htmlspecialchars($_POST["name"], ENT_QUOTES);
       $size = htmlspecialchars($_POST["size"], ENT_QUOTES);
-      if ($_POST["init"] == 1){
+      if ($_POST["formInit"] == 1){
         echo $name . $size;
         }
     ?>
