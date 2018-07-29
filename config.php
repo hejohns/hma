7b5fe03332369b0fd375638d47c362fa3cbcccbe
hejohns <40815177+hejohns@users.noreply.github.com>
2018-07-29 08:09:36 -0400

    Update config.php

--- config.php
+++ config.php
@@ -10,4 +10,8 @@
    $indexUsername = "";
    $indexPassword = "";
    $loaned = $loanedTableName;
+   //using sha256
+   $salt = 'ZAdXqTY5Cm9yGVagkjgo';
+   $hashedPassw = 'ebea74ef71ccd7a00149b8bbc984f2eca28e5a184eab6ad6baee83ea9f8943b7';
+   $hashedUsern = 'a8ef5971486398a3b0cc0ca24bb5f69fc1371eacfc5995743efbaf62784a03b2';
 ?>
