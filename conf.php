d4b30cf9daa8d954cf576925ff81bce0b16451ed
Johnson He <40815177+hejohns@users.noreply.github.com>
2018-09-11 10:23:27 -0400

    Create conf.php

---
+++ conf.php
@@ -0,0 +1,17 @@
+<?php
+//site settings
+   $domain = "https://hmadb.ddns.net";
+   $servername = "localhost";
+   $dbName = "";
+   $dbUsername = "";
+   $dbPassword = "";
+   $checkedOutMaleTableName = "outM";
+   $checkedOutFemaleTableName = "outF";
+   //http auth info
+   $indexUsername = "";
+   $indexPassword = "";
+   //loaned is the main table.
+   $outM = $checkedOutMaleTableName;
+   $outF = $checkedOutFemaleTableName;
+   $loaned = "test";
+?>
